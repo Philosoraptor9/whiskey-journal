0202f1bd98b096a9b71feba469abb18c6ead6a09
Philip Hoover <philhoov90@gmail.com>
2019-01-07 20:50:41 -0700

    Working on google authorization

--- back-end/.next/static/runtime/webpack.js
+++ back-end/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "561529aca9a075b695de";
+/******/ 	var hotCurrentHash = "78104384cad50cf9ce25";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ back-end/server/google.js
@@ -0,0 +1,3 @@
+import passport from 'passport';
+import { OAuth2Strategy as Strategy } from 'passport-google-oauth';
+
