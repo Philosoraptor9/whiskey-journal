f9ef2f1034d279b0a639b56fa149434cad66df7f
Philip Hoover <philhoov90@gmail.com>
2019-01-24 12:15:29 -0700

    admin check written

--- back-end/server/api/admin.js
+++ back-end/server/api/admin.js
@@ -1,3 +1,12 @@
 const router = express.Router()
 
+router.use('/api/v1/admin', (req, res, next) => {
+    if (!req.user || !req.user.isAdmin) {
+        res.status(401).json({ error: 'Unauthorized access' });
+        return;
+    }
+
+    next();
+})
+
 export default router;
